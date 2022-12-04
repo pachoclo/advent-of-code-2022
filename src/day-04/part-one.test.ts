542db90aac2b9412df4ac2e828e6408eee418b5e
Francisco Jaramillo <pacho@live.ca>
2022-12-04 13:57:33 -0500

    day 04 - part one

--- src/day-04/part-one.test.ts
+++ src/day-04/part-one.test.ts
@@ -2,14 +2,19 @@ import { describe, expect, test } from 'vitest'
 import { input } from './input'
 import { partOne } from './part-one'
 
+const sampleInput = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`
+
 describe('day 04 - part one', () => {
   test('solution01: sample', () => {
-    const sampleInput = ``
-
-    expect(partOne(sampleInput)).toBe()
+    expect(partOne(sampleInput)).toBe(2)
   })
 
   test('solution01', () => {
-    expect(partOne(input)).toBe()
+    expect(partOne(input)).toBe(536)
   })
 })
--- src/day-04/part-one.ts
+++ src/day-04/part-one.ts
@@ -1,5 +1,72 @@
 // https://adventofcode.com/2022/day/4
 
 export function partOne(input: string) {
-  const lines = input.split('\n').map((line) => line.trim())
+  let lines = input.split('\n').map((line) => line.trim())
+  let pairs = new Array<Pair>()
+
+  for (const line of lines) {
+    pairs.push(getSectionAssignmentPair(line))
+  }
+
+  let fullyContainingRanges = 0
+
+  for (let pair of pairs) {
+    if (fullyContains(pair)) {
+      fullyContainingRanges += 1
+    }
+  }
+
+  return fullyContainingRanges
+}
+
+// ================
+// Supporting cast
+// ================
+
+type Elf = {
+  sectionIds: number[]
+}
+
+type Pair = {
+  elf01: Elf
+  elf02: Elf
+}
+
+function getSectionAssignmentPair(inputLine: string): Pair {
+  let [range01, range02] = inputLine
+    .split(',')
+    .map((rangeStr) => rangeStr.split('-').map((str) => Number.parseInt(str)))
+
+  return {
+    elf01: {
+      sectionIds: getSectionIdsArray(range01),
+    },
+    elf02: {
+      sectionIds: getSectionIdsArray(range02),
+    },
+  }
+}
+
+function getSectionIdsArray(range: number[]) {
+  let [range01Start, range01End] = range
+  let range01Length = range01End - range01Start + 1
+  let sectionIds = [...Array(range01Length)].map((_, index) => index + range01Start)
+  return sectionIds
+}
+
+function fullyContains(pair: Pair) {
+  let { elf01, elf02 } = pair
+
+  let [longestRange, shorterRange] =
+    elf01.sectionIds.length > elf02.sectionIds.length
+      ? [elf01.sectionIds, elf02.sectionIds]
+      : [elf02.sectionIds, elf01.sectionIds]
+
+  for (let sectionId of shorterRange) {
+    if (!longestRange.includes(sectionId)) {
+      return false
+    }
+  }
+
+  return true
 }
